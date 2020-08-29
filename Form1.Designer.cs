4355f622b535b95b5601466d8433b13cca39c7a8
NatashaDesktop <NatChorba@gmail.com>
2020-08-29 14:17:38 -0400

    Added all the game

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.WorldFrame = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.bot = new System.Windows.Forms.PictureBox();
+            this.playerScoreBox = new System.Windows.Forms.Label();
+            this.botScoreBox = new System.Windows.Forms.Label();
+            this.timerBox = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.ballTimer = new System.Windows.Forms.Timer(this.components);
+            this.WorldFrame.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WorldFrame
+            // 
+            this.WorldFrame.BackColor = System.Drawing.Color.MidnightBlue;
+            this.WorldFrame.Controls.Add(this.player);
+            this.WorldFrame.Controls.Add(this.ball);
+            this.WorldFrame.Controls.Add(this.bot);
+            this.WorldFrame.Location = new System.Drawing.Point(1, 1);
+            this.WorldFrame.Name = "WorldFrame";
+            this.WorldFrame.Size = new System.Drawing.Size(1000, 600);
+            this.WorldFrame.TabIndex = 0;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Cyan;
+            this.player.Location = new System.Drawing.Point(10, 200);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(25, 100);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.GhostWhite;
+            this.ball.Location = new System.Drawing.Point(488, 288);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(24, 24);
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // bot
+            // 
+            this.bot.BackColor = System.Drawing.Color.DarkViolet;
+            this.bot.Location = new System.Drawing.Point(965, 200);
+            this.bot.Name = "bot";
+            this.bot.Size = new System.Drawing.Size(25, 100);
+            this.bot.TabIndex = 0;
+            this.bot.TabStop = false;
+            // 
+            // playerScoreBox
+            // 
+            this.playerScoreBox.AutoSize = true;
+            this.playerScoreBox.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.playerScoreBox.ForeColor = System.Drawing.Color.Cyan;
+            this.playerScoreBox.Location = new System.Drawing.Point(177, 610);
+            this.playerScoreBox.Name = "playerScoreBox";
+            this.playerScoreBox.Size = new System.Drawing.Size(56, 45);
+            this.playerScoreBox.TabIndex = 1;
+            this.playerScoreBox.Text = "00";
+            // 
+            // botScoreBox
+            // 
+            this.botScoreBox.AutoSize = true;
+            this.botScoreBox.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.botScoreBox.ForeColor = System.Drawing.Color.DarkViolet;
+            this.botScoreBox.Location = new System.Drawing.Point(792, 610);
+            this.botScoreBox.Name = "botScoreBox";
+            this.botScoreBox.Size = new System.Drawing.Size(56, 45);
+            this.botScoreBox.TabIndex = 1;
+            this.botScoreBox.Text = "00";
+            // 
+            // timerBox
+            // 
+            this.timerBox.AutoSize = true;
+            this.timerBox.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timerBox.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.timerBox.Location = new System.Drawing.Point(412, 610);
+            this.timerBox.Name = "timerBox";
+            this.timerBox.Size = new System.Drawing.Size(196, 47);
+            this.timerBox.TabIndex = 2;
+            this.timerBox.Text = "Time: 00:00";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 1000;
+            this.gameTimer.Tick += new System.EventHandler(this.TimerTick);
+            // 
+            // ballTimer
+            // 
+            this.ballTimer.Enabled = true;
+            this.ballTimer.Interval = 10;
+            this.ballTimer.Tick += new System.EventHandler(this.GameTick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1004, 701);
+            this.Controls.Add(this.timerBox);
+            this.Controls.Add(this.botScoreBox);
+            this.Controls.Add(this.playerScoreBox);
+            this.Controls.Add(this.WorldFrame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            this.WorldFrame.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bot)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel WorldFrame;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox bot;
+        private System.Windows.Forms.Label playerScoreBox;
+        private System.Windows.Forms.Label botScoreBox;
+        private System.Windows.Forms.Label timerBox;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Timer ballTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        int playerScore;
+        int botScore;
+        readonly int MAXSCORE = 5;
+
+        bool gamePaused = true;
+        int secondsElapsed = 0;
+
+        readonly int playerSpeed = 8;
+        bool playerUp;
+        bool playerDown;
+
+        readonly int botSpeed = 3;
+        
+        int ballXSpeed = 5;
+        int ballYSpeed = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetBall()
+        {
+            ball.Top = 488;
+            ball.Left = 288;
+            ballXSpeed = 5;
+            ballYSpeed = 3;
+            var random = new Random();
+            var ballGoingLeft = ballXSpeed > 0; //just scored on bot, default served to be player
+            if (random.Next(10) >= 3) //70% chance that ball X goes faster than default
+            {
+                ballXSpeed += random.Next(3);
+            }
+            else
+            {
+                ballXSpeed -= random.Next(2);
+            }
+            if (ballGoingLeft) { ballXSpeed = -ballXSpeed; }
+            if (random.Next(10) >= 2) //20% chance it goes toward the player that just lost a point
+            {
+                ballXSpeed = -ballXSpeed;
+            }
+
+            if (random.Next(10) >= 3) //70% chance ball y speed is higher than default
+            {
+                ballYSpeed += random.Next(4);
+            }
+            if (random.Next(10) >= 5) //50% chance of switching direction from default
+            {
+                ballYSpeed = -ballYSpeed;
+            }
+
+        }
+
+#region Collisions
+        bool CheckFloorCollision(PictureBox obj)
+        {
+            if(obj.Location.Y + obj.Height >= WorldFrame.Height)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool CheckCeilingCollision(PictureBox obj)
+        {
+            if (obj.Location.Y <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool CheckLeftCollision(PictureBox obj)
+        {
+            if (obj.Location.X <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool CheckRightCollision(PictureBox obj)
+        {
+            if (obj.Location.X + obj.Width >= WorldFrame.Width)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool CollideWithPaddle(PictureBox paddle)
+        {
+            if (ball.Bounds.IntersectsWith(paddle.Bounds))
+            {
+                return true;
+            }
+            return false;
+        }
+#endregion Collisions
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                case Keys.Up:
+                    playerUp = true;
+                    playerDown = false;
+                    break;
+                case Keys.S:
+                case Keys.Down:
+                    playerDown = true;
+                    playerUp = false;
+                    break;
+                case Keys.Space:
+                    gamePaused = !gamePaused;
+                    break;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                case Keys.Up:
+                    playerUp = false;
+                    break;
+                case Keys.S:
+                case Keys.Down:
+                    playerDown = false;
+                    break;
+            }
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (!gamePaused)
+            {
+                secondsElapsed++;
+                TimeSpan time = TimeSpan.FromSeconds(secondsElapsed);
+                var timeString = time.ToString(@"mm\:ss");
+                timerBox.Text = $"Time: {timeString}";
+            }
+        }
+
+        private void GameTick(object sender, EventArgs e)
+        {
+            if (!gamePaused)
+            {
+                UpdatePlayer();
+                UpdateBot();
+                UpdateBall();
+                CheckGameOver();
+            }
+        }
+
+        private void UpdateBall()
+        {
+            ball.Left -= ballXSpeed;
+            ball.Top -= ballYSpeed;
+
+            if (CheckCeilingCollision(ball) || CheckFloorCollision(ball))
+            {
+                ballYSpeed = -ballYSpeed;
+            }
+
+            if (CheckLeftCollision(ball))
+            {
+                botScore++;
+                botScoreBox.Text = $"{botScore}";
+                ResetBall();
+            }
+            else if (CheckRightCollision(ball))
+            {
+                playerScore++;
+                playerScoreBox.Text = $"{playerScore}";
+                ResetBall();
+            }
+
+            if (CollideWithPaddle(player))
+            {
+                UpdateCollidingBall(player);
+            }
+            else if (CollideWithPaddle(bot))
+            {
+                UpdateCollidingBall(bot);
+            }
+        }
+
+        private void UpdateCollidingBall(PictureBox paddle)
+        {
+            var locDifference = ball.Location.Y - paddle.Location.Y;
+            var ballHasHigherGround = ballYSpeed <= 0; //keep track of whether or not the ball is coming from below or above
+
+            int locRepresentation = (locDifference + 37) / 30; //math time! the differences we care about are at -8,22,52,82,112. Adding 38 to each, they become multiples of 30 and ASSURED greated than 0 difference
+
+            if (!ballHasHigherGround)
+            {
+                locRepresentation = 6 - locRepresentation;
+            }
+
+            switch (locRepresentation)
+            {
+                case 0:
+                    ballXSpeed = (int)(-ballXSpeed*1.3);
+                    ballYSpeed = (int)(-ballYSpeed*1.3);
+                    if (ballYSpeed == 0) ballYSpeed = 2;
+                    break;
+                case 1:
+                    ballXSpeed = -ballXSpeed;
+                    ballYSpeed = (int)(-ballYSpeed * 0.9);
+                    if (ballYSpeed == 0) ballYSpeed = 1;
+                    break;
+                case 3:
+                    ballXSpeed = -ballXSpeed;
+                    ballYSpeed = (int)(ballYSpeed * 1.1);
+                    if (ballYSpeed == 0) ballYSpeed = -1;
+                    break;
+                case 4:
+                    ballXSpeed = (int)(-ballXSpeed * 1.2);
+                    ballYSpeed = (int)(ballYSpeed * 1.5);
+                    if (ballYSpeed == 0) ballYSpeed = -2;
+                    break;
+                default: //for middle hits or wonky shit that I failed to account for...
+                    ballXSpeed = -ballXSpeed;
+                    break;
+            }
+
+            ball.Left -= ballXSpeed;
+            ball.Top -= ballYSpeed;
+        }
+
+        private void UpdateBot()
+        {
+            if (bot.Location.Y < ball.Location.Y)
+            {
+                if (!CheckFloorCollision(bot))
+                {
+                    bot.Top += botSpeed;
+                }
+            }
+            else if (bot.Location.Y + 80 > ball.Location.Y)
+            {
+                if (!CheckCeilingCollision(bot))
+                {
+                    bot.Top -= botSpeed;
+                }
+            }
+        }
+
+        private void UpdatePlayer()
+        {
+            if (playerUp && !CheckCeilingCollision(player))
+            {
+                player.Top -= playerSpeed;
+            }
+            else if (playerDown && !CheckFloorCollision(player))
+            {
+                player.Top += playerSpeed;
+            }
+        }
+
+        private void CheckGameOver()
+        {
+            if (playerScore >= MAXSCORE || botScore >= MAXSCORE)
+            {
+                gameTimer.Stop();
+                ballTimer.Stop();
+                var winnerText = playerScore > botScore ? "You won! I'm pretty impressed!!" : "The bot won! How's it feel being a loser?";
+                MessageBox.Show($"{winnerText}");
+            }
+        }
+    }
+}
